772a4e868ed5a6c57f8cdc99cf25d8cfc4163916
Hemant D <hemantdhage20@gmail.com>
2021-02-23 23:31:48 +0530

    second commit

---
+++ src/main/java/com/jenkins/cicd/Test.java
@@ -0,0 +1,9 @@
+package com.jenkins.cicd;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("#### Demo-ing for Jenkins CICD ####");
+        System.out.println();
+    }
+}
